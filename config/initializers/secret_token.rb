8d27314c186bd4b31bd9e89f0f3b8ff3422584f5
Kurt Thomas <kthomz34@icloud.com>
2015-11-13 01:09:25 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gorlak::Application.config.secret_key_base = 'e32411d7ecb83a22d45b55d5eb212a2005df56b25bb6aaecd5279e20be4b29c1ef642ecad09ce7fb0ae73551b3c6ca5675a6f2610961e4ac2d93085a8f29e9aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gorlak::Application.config.session_store :cookie_store, key: '_gorlak_session'
